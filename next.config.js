9532e369b83170448b06a24c31cb875951e30a1b
Timothy <teecheng5@gmail.com>
2025-12-20 16:43:01 -0800

    Fix deployment config

--- next.config.js
+++ next.config.js
@@ -16,9 +16,6 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
-    dangerouslyAllowSVG: false,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 
   // Optimize production builds
@@ -43,7 +40,6 @@ const nextConfig = {
 
   // Experimental optimizations
   experimental: {
-    optimizeCss: true,
     optimizePackageImports: ['framer-motion'],
   },
 
